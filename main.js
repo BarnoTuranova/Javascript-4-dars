c0bc7053eb44e9c9a769f97f0dbfcca0bc696208
BarnoTuranova <138581787+BarnoTuranova@users.noreply.github.com>
2023-07-30 16:27:08 +0500

    Javascript 4-dars
    24-guruh o'quvchisi

---
+++ main.js
@@ -0,0 +1,45 @@
+/*let headTag = document.head
+let bodyTag =document.body
+let havola= document.documentURI
+let forms = document.forms
+let rasm= document.images*/
+/*console.log(rasm)*/
+
+/*function meniBos(){
+  alert("Button bir marta bosildi")
+}
+
+function meniBosikki(){
+  alert("Button ikki marta bosildi")
+}
+
+function tanlash(event){
+	alert("Siz " +event.target.value+"ni tanladingiz")
+}
+function inputgaBosish(){
+	console.log("Inputga bosildi!")
+}
+function inputdanChiqish(){
+	console.log("Inputdan chiqildi!")
+}
+function yozish(event){
+	console.log(event.target.value)
+}
+
+
+document.querySelector(".matnTegi").addEventListener('copy', function(event){
+	console.log("Diqqat! Diqqat! Sizdan "+event.target.value+ "  Nomli hujjat ko'chirilyapti")
+})*/
+/*function harakatlanish(e){
+	console.log(e.clientX,e.clientY)
+}
+function kirish(){
+	console.log("Hududga kirildi")
+}
+function chiqish(){
+	console.log("Hududdan chiqildi")
+}
+*/
+document.querySelector(".matnTegi").addEventListener('copy', function(event){
+	console.log("Diqqat! Diqqat! Sizdan "+event.target.value+ "  Nomli hujjat ko'chirilyapti")
+})
